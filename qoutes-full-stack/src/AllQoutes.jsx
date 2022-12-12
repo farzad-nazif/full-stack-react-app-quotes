8f02257fe281ab1e51ef1d16975da9362f7e0544
farzad-nazif <farzadnazif.nz@gmail.com>
2022-12-12 23:24:05 +0000

    Trying fetch api from outside of local host

--- qoutes-full-stack/src/AllQoutes.jsx
+++ qoutes-full-stack/src/AllQoutes.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 
 function AllQoutes() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://quotes-api-farzad.glitch.me/quotes")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
   return (
-    <div id='AllQoutes' >AllQoutes</div>
-  )
+    <div id="AllQoutes">
+      {data.map((quote) => {
+        return <p>{quote.quote}</p>;
+      })}
+    </div>
+  );
 }
 
-export default AllQoutes
+export default AllQoutes;
