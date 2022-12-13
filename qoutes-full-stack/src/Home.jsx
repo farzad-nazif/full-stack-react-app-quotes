f415548abb8b1300f7bfcdc1cfc612e02a2cf1e9
farzad-nazif <farzadnazif.nz@gmail.com>
2022-12-13 23:04:55 +0000

    Search Quote page is finished

--- qoutes-full-stack/src/Home.jsx
+++ qoutes-full-stack/src/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
       <h1>Welcome to the quote App</h1>
       <h2>Created by Farzad Nazif</h2>
       <h3>The app is being developed</h3>
-      <h3>This app is very first full stack project of meh</h3>
+      <h3>This app is very first full stack project of me</h3>
     </div>
   );
 }
--- qoutes-full-stack/src/SearchQoutes.jsx
+++ qoutes-full-stack/src/SearchQoutes.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import QuotesBox from "./QuotesBox";
 
 function SearchQoutes() {
+  const [data, setData] = useState([]);
+  const [searchedValue, setSearchedValue] = useState("farzad");
+  useEffect(() => {
+    fetch(
+      `https://quotes-api-farzad.glitch.me/quotes/search?term=${searchedValue}`
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [searchedValue]);
+  const searchHandel = (e) => {
+    let word = e.target.value;
+    setSearchedValue(word);
+  };
   return (
-    <div id='SearchQoutes' >SearchQoutes</div>
-  )
+    <div id="SearchQoutes">
+      <input type="text" onChange={searchHandel} id="searchInput" placeholder="Search in quotes ..." />
+      <div id="AllSearchedQoutes">
+        {searchedValue.length === 0 ? (
+          <></>
+        ) : (
+          data.map((quote) => {
+            return <QuotesBox quote={quote.quote} author={quote.author} />;
+          })
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default SearchQoutes
+export default SearchQoutes;
